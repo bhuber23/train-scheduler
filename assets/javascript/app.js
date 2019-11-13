ca63248a23c669c78784534679b8f546b1eb8f54
Brittany Huber <brittanyleighhuber@gmail.com>
2019-11-12 20:08:12 -0700

    Added form, more styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB-kgqUPaLIhOFiemqBzGaKxc677mGyS1A",
+    authDomain: "train-scheduler-d5c1a.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d5c1a.firebaseio.com",
+    projectId: "train-scheduler-d5c1a",
+    storageBucket: "train-scheduler-d5c1a.appspot.com",
+    messagingSenderId: "532053292521",
+    appId: "1:532053292521:web:32b85b7de7f62bca2c4778"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
